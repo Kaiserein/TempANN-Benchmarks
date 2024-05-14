6e201e4ba6e48ab0f7c173f107bbe971dbf0e800
Kaiserein <1402399785@qq.com>
2024-05-14 18:29:34 +0800

    nsg modified

--- ann_benchmarks/algorithms/faiss_hnsw/module.py
+++ ann_benchmarks/algorithms/faiss_hnsw/module.py
@@ -22,15 +22,15 @@ class FaissHNSW(Faiss):
         self.index.add(X)
         faiss.omp_set_num_threads(1)
 
-    def set_query_arguments(self, ef):
-        faiss.cvar.hnsw_stats.reset()
-        self.index.hnsw.efSearch = ef
+    def set_query_arguments(self, ef): #用于设置搜索查询时使用的参数。ef参数表示搜索时使用的候选项数目。
+        faiss.cvar.hnsw_stats.reset() #重置HNSW统计信息。
+        self.index.hnsw.efSearch = ef #设置搜索时使用的候选项数目为ef。
 
-    def get_additional(self):
-        return {"dist_comps": faiss.cvar.hnsw_stats.ndis}
-
-    def __str__(self):
+    def get_additional(self): #定义了一个get_additional方法，用于获取一些额外的信息，如距离计算的次数。
+        return {"dist_comps": faiss.cvar.hnsw_stats.ndis} #返回一个字典，包含距离计算的次数。
+ 
+    def __str__(self): #定义了类的字符串表示方法，返回一个字符串，描述了当前的索引类型和搜索时使用的候选项数目。
         return "faiss (%s, ef: %d)" % (self.method_param, self.index.hnsw.efSearch)
 
-    def freeIndex(self):
+    def freeIndex(self): #用于释放索引资源。
         del self.p
--- ann_benchmarks/algorithms/faiss_nsg/module.py
+++ ann_benchmarks/algorithms/faiss_nsg/module.py
@@ -4,32 +4,49 @@ import numpy as np
 from ..faiss.module import Faiss
 
 class FaissNSG(Faiss):
-    def __init__(self, metric, R):
-        self.metric = metric
-        self.R = R
-        
-    def fit(self, X):   #X是向量矩阵
-        self.index = faiss.IndexNSGFlat(len(X[0]), self.method_param["R"])
-        
-        self.index.verbose = True   #启用 Faiss 索引的详细输出
-        
-        if self._metric == "angular":   #把angular度量方式换成L2
-            X = X / np.linalg.norm(X, axis=1)[:, np.newaxis]    #如果指定的度量是“angular”（角度距离），则将输入数据 X 规范化。
-        if X.dtype != np.float32:   #确保了输入数据 X 是 float32 类型。
+    def __init__(self, metric, method_param ):
+        self._metric = metric
+        self.method_param = method_param
+
+    def fit(self, X):
+        d = X.shape[1]  # 向量维度
+        if self._metric == 'angular':
+            X /= np.linalg.norm(X, axis=1, keepdims=True)
+        if X.dtype != np.float32:
             X = X.astype(np.float32)
 
-        self.index.add(X)
-        faiss.omp_set_num_threads(1)    #设置 OpenMP 的线程数目为 1
+        self.index = faiss.IndexNSGFlat(d, self.method_param["R"])  # 使用 NSG 算法构建索引
+        self.index.verbose = True
         
+        #if self._metric == "angular":   
+        #    X = X / np.linalg.norm(X, axis=1)[:, np.newaxis]
+        #if X.dtype != np.float32:
+        #    X = X.astype(np.float32)
+
+        if 'K' in self.method_param: #K 最近邻图（KNN graph）中每个节点的邻居数量。默认值为 64
+            self.index.K = self.method_param['K']
+        if 'S' in self.method_param: # NNDescent 算法中的 S 参数，指定了随机探测的样本数目，默认值为 10
+            self.index.nndescent_S = self.method_param['nndescent_S']
+        if 'nndescent_R' in self.method_param: # NNDescent 算法中的 R 参数，指定了最大近邻搜索次数，默认值为 100
+            self.index.nndescent_R = self.method_param['nndescent_R']
+        if 'nndescent_L' in self.method_param: # NNDescent 算法中的 L 参数，指定了最大搜索路径长度，默认值为 GK + 50
+            self.index.nndescent_L = self.method_param['nndescent_L']
+        if 'nndescent_iter' in self.method_param: # NNDescent 算法中的迭代次数。默认值为 10
+            self.index.nndescent_iter = self.method_param['nndescent_iter']
+        if 'R' in self.method_param:
+            self.index.R = self.method_param['R']
+        self.index.train(X)
+        self.index.add(X)
+        faiss.omp_set_num_threads(1)
+
     def set_query_arguments(self, ef):
-        faiss.cvar.nsg_stats.reset()
-        self.index.nsg.efSearch = ef
+        pass  # 在 Faiss 中设置搜索参数通常是在搜索时指定，这里留空即可
 
     def get_additional(self):
-        return {"dist_comps": faiss.cvar.nsg_stats.ndis}
+        return {}
 
     def __str__(self):
-        return "faiss (%s, ef: %d)" % (self.method_param, self.index.nsg.efSearch)
-    
+        return "FaissNSG"
+
     def freeIndex(self):
-        del self.p
+        del self.index
