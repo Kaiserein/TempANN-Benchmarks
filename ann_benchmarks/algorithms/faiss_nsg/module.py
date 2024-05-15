86a7974a54803527fd3b0fd86c2ba72d78105554
Kaiserein <1402399785@qq.com>
2024-05-15 22:02:48 +0800

    2024/5/15 night

--- ann_benchmarks/algorithms/faiss_nsg/module.py
+++ ann_benchmarks/algorithms/faiss_nsg/module.py
@@ -4,28 +4,23 @@ import numpy as np
 from ..faiss.module import Faiss
 
 class FaissNSG(Faiss):
-    def __init__(self, metric, method_param ):
+    def __init__(self, metric, method_param):
         self._metric = metric
         self.method_param = method_param
 
     def fit(self, X):
         d = X.shape[1]  # 向量维度
-        if self._metric == 'angular':
+        if self._metric == 'angular':   #归一化处理
             X /= np.linalg.norm(X, axis=1, keepdims=True)
         if X.dtype != np.float32:
             X = X.astype(np.float32)
 
-        self.index = faiss.IndexNSGFlat(d, self.method_param["R"])  # 使用 NSG 算法构建索引
+        self.index = faiss.IndexNSGFlat(d, self.method_param["R"])  # 使用 NSG 算法构建索引, R代表邻居数量
         self.index.verbose = True
-        
-        #if self._metric == "angular":   
-        #    X = X / np.linalg.norm(X, axis=1)[:, np.newaxis]
-        #if X.dtype != np.float32:
-        #    X = X.astype(np.float32)
 
         if 'K' in self.method_param: #K 最近邻图（KNN graph）中每个节点的邻居数量。默认值为 64
             self.index.K = self.method_param['K']
-        if 'S' in self.method_param: # NNDescent 算法中的 S 参数，指定了随机探测的样本数目，默认值为 10
+        if 'nndescent_S' in self.method_param: # NNDescent 算法中的 S 参数，指定了随机探测的样本数目，默认值为 10
             self.index.nndescent_S = self.method_param['nndescent_S']
         if 'nndescent_R' in self.method_param: # NNDescent 算法中的 R 参数，指定了最大近邻搜索次数，默认值为 100
             self.index.nndescent_R = self.method_param['nndescent_R']
@@ -33,20 +28,22 @@ class FaissNSG(Faiss):
             self.index.nndescent_L = self.method_param['nndescent_L']
         if 'nndescent_iter' in self.method_param: # NNDescent 算法中的迭代次数。默认值为 10
             self.index.nndescent_iter = self.method_param['nndescent_iter']
-        if 'R' in self.method_param:
-            self.index.R = self.method_param['R']
+        if 'Search_L' in self.method_param: #搜索路径长度
+            self.index.Search_L = self.method_param['Search_L']
+        if 'C' in self.method_param: #候选集大小
+            self.index.C = self.method_param['C']
         self.index.train(X)
         self.index.add(X)
         faiss.omp_set_num_threads(1)
 
-    def set_query_arguments(self, ef):
-        pass  # 在 Faiss 中设置搜索参数通常是在搜索时指定，这里留空即可
+    def set_query_arguments(self, ef): #没有需要额外设置的参数，所有参数都在fit阶段设置完毕
+        pass
 
-    def get_additional(self):
+    def get_additional(self): #没有额外参数，因此不需要返回任何内容
         return {}
 
     def __str__(self):
         return "FaissNSG"
 
-    def freeIndex(self):
+    def freeIndex(self): #释放占用的资源
         del self.index
